25d36dd835d85039735e5e28e53bdfcdeb1edc9d
NaveenNizam <naveennizam01@gmail.com>
2022-08-18 02:30:28 -0700

    Quiz

--- js/app.js
+++ js/app.js
@@ -13,11 +13,11 @@ const total = document.querySelector("#total")
 let Count = 0;
 let score = 0;
 
-const quiz = [
+const quiz = [   //&emsp; 4 tab spaces , &ensp; 2 tab spaces
     {
         Question: `Q1: let schema = ["hi","friend",null, null, "goodbye"] <br>
                            schema = schema.filter(function(n) { <br>
-                                    return n <br>
+                            &emsp;&emsp;  return n <br>
                                     });`,
         a: `['hi', 'friend', 'goodbye']`,
         b: `['hi', 'friend', 'null', 'null', 'goodbye']`,
@@ -27,26 +27,26 @@ const quiz = [
     },
     {
         Question: `Q2: function printMe() {  <br>
-            console.log('print me'); <br>
+            &ensp; console.log('print me'); <br>
           }    <br>
           function test() { <br>
-            console.log('test'); <br>
+            &emsp; &emsp; console.log('test'); <br>
           } <br>
           setTimeout(printMe, 0); <br>
           test();`, 
 
         a: `print me <br>
-                test`,
+        &emsp;   test`,
         b: `test <br>
-            print me`,
+        &emsp; print me`,
         c: `Refrence Error`,
         d: `Syntax Error`,
         ans: `ans2`
     },
     {
-        Question: `Q3: function fn(){
-                          fn()
-                        }
+        Question: `Q3: function fn(){ <br>
+            &emsp;  &emsp;  fn() <br>
+                        } <br>
                        fn()`,
         a: `Syntax Error`,
         b: `Range Error`,
@@ -54,17 +54,17 @@ const quiz = [
         d: `null`,
         ans: `ans2`
     }, {
-        Question: `Q4: function f1() {
-                            console.log('f1');
-                        }
-                        function f2() {
-                            console.log('f2');
-                        }
-                        function main() {
-                            console.log('main');
-                            f1();
-                            f2();
-                        }
+        Question: `Q4: function f1() {  
+            &emsp; &ensp; console.log('f1');  &emsp;
+                        } <br>
+                        &emsp; &ensp; function f2() {
+                            &emsp; &ensp; console.log('f2');   &emsp;
+                        } <br>
+                        &emsp; &ensp; function main() { <br>
+                          &emsp; &emsp;  &emsp;&emsp; console.log('main'); <br>
+                          &emsp; &emsp;  &emsp;&emsp; f1(); <br>
+                          &emsp; &emsp;  &emsp;&emsp; f2(); <br>
+                        } <br>
                         main();`,
         a: `Error`,
         b: `f1
@@ -79,193 +79,118 @@ const quiz = [
         ans: `ans4`
     },
     {
-        Question: `Q5: getage(2000)
-                       function getage(year){
-                           console.log(2022 - year)
+        Question: `Q5: getage(2000)     <br>      <br>
+                       function getage(year){ <br>
+                        &emsp; &emsp; console.log(2022 - year) <br>
                        }  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        a: ` 20 `,
+        b: ` 21 `,
+        c: ` 22 `,
+        d: ` Invalid`,
+        ans: `ans3`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q6: let r = () => console.log("Hello World!") <br> <br> r(); `,
+        a: ` Hello World! `,
+        b: ` Syntax Error`,
+        c: ` Reference Error `,
+        d: ` r is not defined`,
+        ans: `ans1`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q7: r(); <br> <br> let r = () => console.log("Hello World!")  `,
+        a: ` Hello World! `,
+        b: ` Syntax Error`,
+        c: ` Reference Error `,
+        d: ` r is not defined`,
+        ans: `ans4`
+        
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q8: agree() <br> 
+        function agree() { <br> <br>
+           &emsp; &emsp; console.log("Imagination is greater than detail.")  <br>
+        } `,
+        a: ` Null `,
+        b: ` Syntax Error`,
+        c: ` Imagination is greater than detail.  `,
+        d: ` agree is not defined`,
+        ans: `ans3`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q9: class student { <br>
+            &emsp;  &emsp; constructor() { <br>
+                &emsp; &emsp; &emsp;  console.log("Powerful dreams inspire powerful action.") <br>
+                &emsp; &emsp;} <br>
+        } <br>
+        let real = new student()  `,
+        a: ` Syntax Error `,
+        b: ` class is not defined `,
+        c: ` real is not defined `,
+        d: ` Powerful dreams inspire powerful action. `,
+        ans: `ans4`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q10: class student { <br>
+            &emsp;   &emsp; prototype() { <br>
+                &emsp;&emsp;&emsp;  console.log("There is nothing impossible to they who will try.")
+                <br>    } <br>
+        } <br>
+        let real = new student() <br>
+        real.prototype()  `,
+        a: ` Syntax Error `,
+        b: ` class is not defined `,
+        c: ` real is not defined `,
+        d: ` There is nothing impossible to they who will try.`,
+        ans: `ans4`
+        
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q11: class student { <br>
+            &emsp;  &emsp;  static hello() { <br>
+                &emsp;  &emsp;  &emsp;console.log("Don't tell people your plans.") <br>
+             } <br>
+         } <br>
+         student.hello() `,
+         a: ` Syntax Error `,
+         b: ` class is not defined `,
+         c: ` Don't tell people your plans. `,
+         d: ` Refrence Error`,
+         ans: `ans3`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q12: Is JavaScript ignores extra spaces? `,
+        a: ` True`,
+        b: ` False`,
+        c: `  undefined`,
+        d: `   null`,
+        
+        ans: `ans1`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q13: Why so Java and JavaScript have similar name? `,
+        a: ` They both support object oriented programming`,
+        b: ` The syntax of JavaScript is loosely based on Java syntax`,
+        c: `  They both are same`,
+        d: ` none of above `,
+        ans: `ans2`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q14: console.log(018 - 015) `,
+        a: ` 3`,
+        b: ` 4`,
+        c: ` 5`,
+        d: ` None of Above`,
+        ans: `ans3`
     },
     {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
-    },
-    {
-        Question: `Q5:  `,
-        a: ` `,
-        b: ` `,
-        c: ` `,
-        d: ` `,
-        ans: `ans`
+        Question: `Q15: console.log(('b' + 'a' + + 'a' + 'a').toLowerCase()); `,
+        a: `baaa `,
+        b: ` baa`,
+        c: ` ba aa`,
+        d: `banana `,
+        ans: `ans4`
     }
-
 
 ]
 
@@ -330,7 +255,7 @@ document.querySelector("#next").addEventListener('click', () => {
     }
 });
 
-let min = 15;
+let min = 10;
 let time = min * 60; // min * 60
 let update = () => {
     let minute = Math.floor(time / 60) //Quotient
